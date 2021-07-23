59f38ea7054367e1026388aef8de15e81b968116
Dakota L Martinez <dakotaleemusic@gmail.com>
2021-07-22 21:22:41 -0700

    initial commit

---
+++ lib/intro_to_databases_and_activerecord.rb
@@ -0,0 +1,6 @@
+require "intro_to_databases_and_activerecord/version"
+
+module IntroToDatabasesAndActiverecord
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/intro_to_databases_and_activerecord/version.rb
@@ -0,0 +1,3 @@
+module IntroToDatabasesAndActiverecord
+  VERSION = "0.1.0"
+end
